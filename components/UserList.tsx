4c2eb6f76cf2979390436b09f3a24566871be744
fox-hound-hayato <hayato.murakami@fox-hound.jp>
2025-04-09 16:37:52 +0900

    day2 タスク1-1

---
+++ components/UserList.tsx
@@ -0,0 +1,22 @@
+import { User } from "../types/User";
+import UserCard from "./UserCard";
+
+// 2.UserListPropsインターフェースの定義
+interface UserListProps {
+  users: User[];
+} 
+
+// 3.UserListPropsで定義した内容を受け取る　
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <div>
+     {/* 4.UserListPropsで取得したユーザー情報を利用して、UserCardコンポーネントを呼び出す（map利用） */}
+     {/* UserList コンポーネント内で fetch は⾏わない */}
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
