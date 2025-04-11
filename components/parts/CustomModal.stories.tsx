ef83989413e8f44e6d129aafa6e09daa6835a819
fox-hound-hayato <hayato.murakami@fox-hound.jp>
2025-04-11 15:17:34 +0900

    day3タスク3-2

---
+++ components/parts/CustomModal.stories.tsx
@@ -0,0 +1,46 @@
+// components/parts/CustomModal.stories.tsx
+
+import React, { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import CustomModal from "./CustomModal";
+import CustomButton from "./CustomButton";
+import { Box } from "@mui/material";
+
+// TODO: メタデータ
+const meta: Meta<typeof CustomModal> = {
+    title: "Components/CustomModal",
+    component: CustomModal,
+    tags: ["autodocs"],
+  };
+
+export default meta;  
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof CustomModal>;
+
+// TODO: デフォルトストーリーの作成
+export const Default: Story = {
+  render: () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <Box>
+        {/* // TODO: クリックでモーダル開閉させる */}
+        <CustomButton variantType="primary" onClick={() => setOpen(true)}>
+          モーダルを開く
+        </CustomButton>
+        <CustomModal
+        // TODO: Propを渡す
+        open={open} // モーダルの開閉状態を管理するためのstateを渡す
+        title="モーダルのタイトル" // モーダルのタイトルを渡す
+        content="モーダルの内容" // モーダルの内容を渡す
+        onClose={() => setOpen(false)}// onCloceはsetOpenにfalseを渡す
+        onConfirm={() => { // onConfirmはalert()を使ってクリックしたことを知らせる
+            alert("クリックされました！"); 
+            setOpen(false); //// setOpenにfalseを渡す モーダルを閉じる
+        }}
+        />
+      </Box>
+    );
+  },
+};
